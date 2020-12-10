8f332aaaff15fda50dd815f761e713f166a43a02
Jahmelia Atkinson <jahmeliaatkinson@yahoo.com>
2020-12-10 02:03:14 +0000

    first commit

---
+++ ProductRestAPI/src/main/java/com/tcs/productrestapi/exception/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tcs.productrestapi.exception;
+
+public class ProductNotFoundException extends Exception {
+
+	public ProductNotFoundException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+}
---
+++ ProductRestAPI/src/main/java/com/tcs/productrestapi/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.tcs.productrestapi.service;
+
+import java.util.Optional;
+
+import com.tcs.productrestapi.model.Product;
+
+public interface ProductService {
+	
+	public Product createProduct(Product product);
+	public Optional<Product> getProductById(int id);
+	public void deleteProduct(int id);
+	public Optional<java.util.List<Product>> getProducts();
+	public Optional<java.util.List<Product>> getProductsByCategory(String catName);
+
+}
